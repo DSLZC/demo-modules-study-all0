8a52978808100de16d6085a6558cbb732d3d4a2c
dongsilin <dongsilin@51ishare.com>
2016-12-30 13:46:23 +0800

    添加 js插件工程

---
+++ demo-plugins-js/src/main/java/com/web/modules/DemoModulesWebApplication.java
@@ -0,0 +1,12 @@
+package com.web.modules;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoModulesWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoModulesWebApplication.class, args);
+	}
+}
